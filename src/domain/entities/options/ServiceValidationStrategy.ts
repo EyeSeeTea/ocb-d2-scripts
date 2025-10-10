894146510d38dbc710aabbd2854d8b61027819d7
Eduardo Peredo Rivero <jrperedo@gmail.com>
2025-10-09 19:51:41 -0500

    allow partial service code matches using includes

--- src/domain/entities/options/ServiceValidationStrategy.ts
+++ src/domain/entities/options/ServiceValidationStrategy.ts
@@ -107,7 +107,7 @@ export class ServiceValidationStrategy {
         switch (pattern.type) {
             case "standard_pattern": {
                 const { optionName, service } = pattern;
-                const serviceCode = settings.services.find(s => s.code === service)?.code;
+                const serviceCode = settings.services.find(s => service.includes(s.code))?.code;
                 if (!serviceCode)
                     return [
                         this.generateValidationError({
@@ -194,7 +194,7 @@ export class ServiceValidationStrategy {
 
     private validateStandardPattern(name: string): Maybe<StandardPattern> {
         // [Option Name] ([service acronym])
-        const pattern = /^(?<optionName>.+?)\s*\(\s*(?<service>[A-Z0-9_]+)\s*\)$/;
+        const pattern = /^(?<optionName>.+?)\s*\(\s*(?<service>[^)]+)\s*\)$/;
         const match = name.match(pattern);
         if (!match || !match.groups) return undefined;
 
